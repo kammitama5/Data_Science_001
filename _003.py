ce88a0ab468977859b828a8bcd7d85c47b8a5c9d
kammitama5 <krystal.maughan@gmail.com>
2016-11-08 11:11:27 -0800

    string manip string formatting

---
+++ _003.py
@@ -0,0 +1,11 @@
+grocery_list = {
+    'potato' : 4.00,
+    'ice-cream' : 5,
+    'wantons' : 12
+}
+
+grocery_list1 = 'I bought one potato at {} dollars each'
+
+print(grocery_list1.format(grocery_list['potato']))
+
+# I bought one potato at 4.0 dollars each
