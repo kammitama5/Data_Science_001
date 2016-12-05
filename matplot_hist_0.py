9bdfbf269311c6f3ce0127ee8cd9e2562d974a70
kammitama5 <krystal.maughan@gmail.com>
2016-12-05 15:32:30 -0600

    matplot_histograms

---
+++ matplot_hist_0.py
@@ -0,0 +1,27 @@
+
+# coding: utf-8
+
+# In[1]:
+
+import matplotlib.pyplot as plt
+
+
+# In[2]:
+
+values = [0, 0.6, 1.4, 1.6, 2.2, 2.5, 2.6, 3.2, 3.5, 3.9, 4.2, 6]
+
+
+# In[3]:
+
+plt.hist(values, bins = 3)
+
+
+# In[4]:
+
+plt.show()
+
+
+# In[ ]:
+
+
+
