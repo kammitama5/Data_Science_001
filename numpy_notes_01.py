73e03c5ba6e3db21bd78074f2413c181ecb10e8f
kammitama5 <krystal.maughan@gmail.com>
2016-12-05 12:26:31 -0600

    numpy notes => jupyter notebook

---
+++ numpy_notes_01.py
@@ -0,0 +1,36 @@
+
+# coding: utf-8
+
+# In[7]:
+
+import numpy as np
+
+# height = np.round(np.random.normal(1.75, 0.20, 5000), 2)
+# weight = np.round(np.random.normal(60.32, 15, 5000), 2)
+# np_city = np.column_stack((height, weight))
+
+
+# In[2]:
+
+# np.mean(np_city[:,0]) => mean
+
+
+# In[3]:
+
+#np.median(np_city[:,0]) => median
+
+
+# In[4]:
+
+#np.corrcoef(np_city[:,0], np_city[:,1]) => check if values are correlated
+
+
+# In[5]:
+
+#np.std(np_city[:,0]) => standard deviation
+
+
+# In[ ]:
+
+
+
