40b8cd53d07214f726e6ff76fee1970032c8c419
kammitama5 <krystal.maughan@gmail.com>
2016-12-05 12:07:37 -0600

    test to solve four

---
+++ numpy_test.py
@@ -0,0 +1,32 @@
+
+# coding: utf-8
+
+# In[1]:
+
+import numpy as np
+
+
+# In[2]:
+
+np_mat = np.array([[1,2], [3, 4], [5, 6]])
+
+
+# In[3]:
+
+np_mat * 2
+
+
+# In[4]:
+
+np_mat + np.array([10, 10])
+
+
+# In[5]:
+
+np_mat + np_mat
+
+
+# In[ ]:
+
+
+
